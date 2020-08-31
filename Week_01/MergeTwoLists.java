9a720c88d66cb976c3b84cd6930afbfb5e7251f6
Michelangelo-apple <sunshine_apple@126.com>
2020-09-01 00:01:04 +0800

    增加内部类

--- Week_01/MergeTwoLists.java
+++ Week_01/MergeTwoLists.java
@@ -50,4 +50,22 @@ public class MergeTwoLists {
         }
 
     }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
 }
