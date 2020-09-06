543b83f0a1b0a2ab555561abbe6f68413ca2ffc7
Michelangelo-apple <sunshine_apple@126.com>
2020-09-06 23:53:46 +0800

    更新ignore文件

--- Week_01/MyCircularDeque.java
+++ Week_01/MyCircularDeque.java
@@ -10,13 +10,17 @@ public class MyCircularDeque {
 
     private int tail;
 
-    /** Initialize your data structure here. Set the size of the deque to be k. */
+    /**
+     * Initialize your data structure here. Set the size of the deque to be k.
+     */
     public MyCircularDeque(int k) {
         capacity = k + 1; // 因为环形队列需要一个空位判断队列为空
         data = new int[capacity];
     }
 
-    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    /**
+     * Adds an item at the front of Deque. Return true if the operation is successful.
+     */
     public boolean insertFront(int value) {
         if (isFull()) {
             return false;
@@ -27,7 +31,9 @@ public class MyCircularDeque {
         return true;
     }
 
-    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    /**
+     * Adds an item at the rear of Deque. Return true if the operation is successful.
+     */
     public boolean insertLast(int value) {
         if (isFull()) {
             return false;
@@ -38,7 +44,9 @@ public class MyCircularDeque {
         return true;
     }
 
-    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    /**
+     * Deletes an item from the front of Deque. Return true if the operation is successful.
+     */
     public boolean deleteFront() {
         if (isEmpty()) {
             return false;
@@ -48,7 +56,9 @@ public class MyCircularDeque {
         return true;
     }
 
-    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    /**
+     * Deletes an item from the rear of Deque. Return true if the operation is successful.
+     */
     public boolean deleteLast() {
         if (isEmpty()) {
             return false;
@@ -58,7 +68,9 @@ public class MyCircularDeque {
         return true;
     }
 
-    /** Get the front item from the deque. */
+    /**
+     * Get the front item from the deque.
+     */
     public int getFront() {
         if (isEmpty()) {
             return -1;
@@ -67,7 +79,9 @@ public class MyCircularDeque {
         return data[head];
     }
 
-    /** Get the last item from the deque. */
+    /**
+     * Get the last item from the deque.
+     */
     public int getRear() {
         if (isEmpty()) {
             return -1;
@@ -76,12 +90,16 @@ public class MyCircularDeque {
         return data[(tail + capacity - 1) % capacity];
     }
 
-    /** Checks whether the circular deque is empty or not. */
+    /**
+     * Checks whether the circular deque is empty or not.
+     */
     public boolean isEmpty() {
         return head == tail;
     }
 
-    /** Checks whether the circular deque is full or not. */
+    /**
+     * Checks whether the circular deque is full or not.
+     */
     public boolean isFull() {
         return (tail + 1) % capacity == head;
     }
